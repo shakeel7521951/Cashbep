599f56cd1cc7d5a5cbeb04d9a2ecaf08daf2645f
Muhammad Shakeel <149057002+shakeel7521951@users.noreply.github.com>
2025-08-21 12:50:05 +0500

    Update baseUrl.js

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,4 +1,5 @@
 // export const baseUrl = `http://localhost:8000`;
-export const baseUrl = `https://bmx-backend.onrender.com`;
+// export const baseUrl = `https://bmx-backend.onrender.com`;
+export const baseUrl = `https://bmx-backend-nqxw.onrender.com`;
 // export const baseUrl = `https://cashbep-backend.onrender.com`;
 
